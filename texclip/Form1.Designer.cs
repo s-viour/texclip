c4ae0425e3c658f69635f5edc48438b4ff8a48a9
Matt Wyatt <mwyatt1000@gmail.com>
2021-11-06 20:49:20 -0400

    change background color to white

--- texclip/Form1.Designer.cs
+++ texclip/Form1.Designer.cs
@@ -54,6 +54,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(679, 480);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.textBox1);
